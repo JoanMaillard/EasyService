baf5410362f08837a39e1a5bfac1e8b264e59fda
joan <joan@nerdgautica.net>
2021-01-05 14:12:09 +0100

    Implemented DBException

---
+++ src/main/java/BDR6/EasyService/DBConn/DBException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BDR6.EasyService.DBConn;
+
+/**
+ *
+ * @author Joan
+ */
+public class DBException extends Exception {
+    public DBException(String msg) {
+        super(msg);
+        this.printStackTrace();
+    }
+}
