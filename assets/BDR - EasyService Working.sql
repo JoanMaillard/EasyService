18ee25d2433bd1dc55a71e4f166ed568b8eb3192
Joan Maillard <joan@nerdgautica.net>
2021-01-17 11:22:53 +0100

    Removed pesky ` character

--- assets/BDR - EasyService Working.sql
+++ assets/BDR - EasyService Working.sql
@@ -15,7 +15,7 @@ CREATE SCHEMA IF NOT EXISTS EasyService DEFAULT CHARACTER SET utf8mb4 ;
 USE EasyService ;
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Staff`
+-- Table EasyService.Staff
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS EasyService.Staff (
   id INT NOT NULL AUTO_INCREMENT,
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS EasyService.Staff (
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Service`
+-- Table EasyService.Service
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS EasyService.Service (
   id INT NOT NULL AUTO_INCREMENT,
@@ -39,18 +39,18 @@ CREATE TABLE IF NOT EXISTS EasyService.Service (
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Table`
+-- Table EasyService.Table
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`Table` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `ouvertFerme` TINYINT NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+CREATE TABLE IF NOT EXISTS EasyService.Table (
+  id INT NOT NULL AUTO_INCREMENT,
+  ouvertFerme TINYINT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);
 
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Commande`
+-- Table EasyService.Commande
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS EasyService.Commande (
   id INT NOT NULL AUTO_INCREMENT,
@@ -59,29 +59,29 @@ CREATE TABLE IF NOT EXISTS EasyService.Commande (
   idService INT NOT NULL,
   idTable INT NOT NULL,
   PRIMARY KEY (id),
-  INDEX `FK_Commande_idStaff_idx` (`idStaff` ASC) VISIBLE,
-  INDEX `FK_Commande_idService_idx` (`idService` ASC) VISIBLE,
-  INDEX `FK_Commande_idTable_idx` (`idTable` ASC) VISIBLE,
-  CONSTRAINT `FK_Commande_idStaff`
-    FOREIGN KEY (`idStaff`)
-    REFERENCES `EasyService`.`Staff` (`id`)
+  INDEX FK_Commande_idStaff_idx (idStaff ASC) VISIBLE,
+  INDEX FK_Commande_idService_idx (idService ASC) VISIBLE,
+  INDEX FK_Commande_idTable_idx (idTable ASC) VISIBLE,
+  CONSTRAINT FK_Commande_idStaff
+    FOREIGN KEY (idStaff)
+    REFERENCES EasyService.Staff (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `FK_Commande_idService`
-    FOREIGN KEY (`idService`)
-    REFERENCES `EasyService`.`Service` (`id`)
+  CONSTRAINT FK_Commande_idService
+    FOREIGN KEY (idService)
+    REFERENCES EasyService.Service (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `FK_Commande_idTable`
-    FOREIGN KEY (`idTable`)
-    REFERENCES `EasyService`.`Table` (`id`)
+  CONSTRAINT FK_Commande_idTable
+    FOREIGN KEY (idTable)
+    REFERENCES EasyService.Table (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Categorie`
+-- Table EasyService.Categorie
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS EasyService.Categorie (
   id INT NOT NULL AUTO_INCREMENT,
@@ -92,7 +92,7 @@ CREATE TABLE IF NOT EXISTS EasyService.Categorie (
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Produit`
+-- Table EasyService.Produit
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS EasyService.Produit (
   id INT NOT NULL AUTO_INCREMENT,
@@ -100,19 +100,19 @@ CREATE TABLE IF NOT EXISTS EasyService.Produit (
   prixVente FLOAT NOT NULL,
   idCategorie INT NOT NULL,
   PRIMARY KEY (id),
-  INDEX `fk_Produit_Categorie1_idx` (`idCategorie` ASC) VISIBLE,
-  UNIQUE INDEX `nom_UNIQUE` (`nom` ASC) VISIBLE,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  CONSTRAINT `FK_Produit_idCategorie`
-    FOREIGN KEY (`idCategorie`)
-    REFERENCES `EasyService`.`Categorie` (`id`)
+  INDEX fk_Produit_Categorie1_idx (idCategorie ASC) VISIBLE,
+  UNIQUE INDEX nom_UNIQUE (nom ASC) VISIBLE,
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE,
+  CONSTRAINT FK_Produit_idCategorie
+    FOREIGN KEY (idCategorie)
+    REFERENCES EasyService.Categorie (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
   
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Commande_Produit`
+-- Table EasyService.Commande_Produit
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS EasyService.Commande_Produit (
   idCommande INT NOT NULL,
@@ -126,164 +126,164 @@ CREATE TABLE IF NOT EXISTS EasyService.Commande_Produit (
     REFERENCES EasyService.Commande (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `FK_Commande_Produit_idProduit`
-    FOREIGN KEY (`idProduit`)
-    REFERENCES `EasyService`.`Produit` (`id`)
+  CONSTRAINT FK_Commande_Produit_idProduit
+    FOREIGN KEY (idProduit)
+    REFERENCES EasyService.Produit (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`ArticleStock`
+-- Table EasyService.ArticleStock
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`ArticleStock` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `nom_UNIQUE` (`nom` ASC) VISIBLE,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+CREATE TABLE IF NOT EXISTS EasyService.ArticleStock (
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX nom_UNIQUE (nom ASC) VISIBLE,
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE);
 
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`LotArticle`
+-- Table EasyService.LotArticle
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`LotArticle` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `datePeremption` DATE NOT NULL,
-  `nombrePortions` INT NOT NULL,
-  `prixAchat` FLOAT NOT NULL,
-  `idArticleStock` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_LotArticle_ArticleStock1_idx` (`idArticleStock` ASC) VISIBLE,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  CONSTRAINT `FK_LotArticle_idArticleStock`
-    FOREIGN KEY (`idArticleStock`)
-    REFERENCES `EasyService`.`ArticleStock` (`id`)
+CREATE TABLE IF NOT EXISTS EasyService.LotArticle (
+  id INT NOT NULL AUTO_INCREMENT,
+  datePeremption DATE NOT NULL,
+  nombrePortions INT NOT NULL,
+  prixAchat FLOAT NOT NULL,
+  idArticleStock INT NOT NULL,
+  PRIMARY KEY (id),
+  INDEX fk_LotArticle_ArticleStock1_idx (idArticleStock ASC) VISIBLE,
+  UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE,
+  CONSTRAINT FK_LotArticle_idArticleStock
+    FOREIGN KEY (idArticleStock)
+    REFERENCES EasyService.ArticleStock (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
   
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Log`
+-- Table EasyService.Log
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`Log` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATETIME NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS EasyService.Log (
+  id INT NOT NULL AUTO_INCREMENT,
+  date DATETIME NOT NULL,
+  PRIMARY KEY (id));
 
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`HistoriqueStaff`
+-- Table EasyService.HistoriqueStaff
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`HistoriqueStaff` (
-  `idLog` INT NOT NULL,
-  `debut` DATETIME NOT NULL,
-  `fin` DATETIME NOT NULL,
-  `idStaff` INT NOT NULL,
-  PRIMARY KEY (`idLog`),
-  INDEX `FK_HistoriqueStaff_idStaff_idx` (`idStaff` ASC) VISIBLE,
-  INDEX `FK_HistoriqueStaff_idLog_idx` (`idLog` ASC) VISIBLE,
-  CONSTRAINT `FK_HistoriqueStaff_idStaff`
-    FOREIGN KEY (`idStaff`)
-    REFERENCES `EasyService`.`Staff` (`id`)
+CREATE TABLE IF NOT EXISTS EasyService.HistoriqueStaff (
+  idLog INT NOT NULL,
+  debut DATETIME NOT NULL,
+  fin DATETIME NOT NULL,
+  idStaff INT NOT NULL,
+  PRIMARY KEY (idLog),
+  INDEX FK_HistoriqueStaff_idStaff_idx (idStaff ASC) VISIBLE,
+  INDEX FK_HistoriqueStaff_idLog_idx (idLog ASC) VISIBLE,
+  CONSTRAINT FK_HistoriqueStaff_idStaff
+    FOREIGN KEY (idStaff)
+    REFERENCES EasyService.Staff (id)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
-  CONSTRAINT `FK_HistoriqueStaff_idLog`
-    FOREIGN KEY (`idLog`)
-    REFERENCES `EasyService`.`Log` (`id`)
+  CONSTRAINT FK_HistoriqueStaff_idLog
+    FOREIGN KEY (idLog)
+    REFERENCES EasyService.Log (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`Addition`
+-- Table EasyService.Addition
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`Addition` (
-  `idLog` INT NOT NULL,
-  `coutPrixTotal` DOUBLE NOT NULL,
-  `estPaye` TINYINT NOT NULL,
-  `Commande_id` INT NOT NULL,
-  PRIMARY KEY (`idLog`),
-  INDEX `FK_Addition_idLog_idx` (`idLog` ASC) VISIBLE,
-  INDEX `fk_Addition_Commande1_idx` (`Commande_id` ASC) VISIBLE,
-  CONSTRAINT `FK_Addition_idLog`
-    FOREIGN KEY (`idLog`)
-    REFERENCES `EasyService`.`Log` (`id`)
+CREATE TABLE IF NOT EXISTS EasyService.Addition (
+  idLog INT NOT NULL,
+  coutPrixTotal DOUBLE NOT NULL,
+  estPaye TINYINT NOT NULL,
+  Commande_id INT NOT NULL,
+  PRIMARY KEY (idLog),
+  INDEX FK_Addition_idLog_idx (idLog ASC) VISIBLE,
+  INDEX fk_Addition_Commande1_idx (Commande_id ASC) VISIBLE,
+  CONSTRAINT FK_Addition_idLog
+    FOREIGN KEY (idLog)
+    REFERENCES EasyService.Log (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_Addition_Commande1`
-    FOREIGN KEY (`Commande_id`)
-    REFERENCES `EasyService`.`Commande` (`id`)
+  CONSTRAINT fk_Addition_Commande1
+    FOREIGN KEY (Commande_id)
+    REFERENCES EasyService.Commande (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
   
 -- -----------------------------------------------------
--- Table `EasyService`.`EcritureStock`
+-- Table EasyService.EcritureStock
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`EcritureStock` (
-  `idLog` INT NOT NULL,
-  `idCommande` INT NOT NULL,
-  PRIMARY KEY (`idLog`, `idCommande`),
-  INDEX `FK_EcritureStock_idLog_idx` (`idLog` ASC) VISIBLE,
-  INDEX `FK_EcritureStock_idCommande_idx` (`idCommande` ASC) VISIBLE,
-  CONSTRAINT `FK_EcritureStock_idLog`
-    FOREIGN KEY (`idLog`)
-    REFERENCES `EasyService`.`Log` (`id`)
+CREATE TABLE IF NOT EXISTS EasyService.EcritureStock (
+  idLog INT NOT NULL,
+  idCommande INT NOT NULL,
+  PRIMARY KEY (idLog, idCommande),
+  INDEX FK_EcritureStock_idLog_idx (idLog ASC) VISIBLE,
+  INDEX FK_EcritureStock_idCommande_idx (idCommande ASC) VISIBLE,
+  CONSTRAINT FK_EcritureStock_idLog
+    FOREIGN KEY (idLog)
+    REFERENCES EasyService.Log (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `FK_EcritureStock_idCommande`
-    FOREIGN KEY (`idCommande`)
-    REFERENCES `EasyService`.`Commande` (`id`)
+  CONSTRAINT FK_EcritureStock_idCommande
+    FOREIGN KEY (idCommande)
+    REFERENCES EasyService.Commande (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
  
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`ArticleStock_Produit`
+-- Table EasyService.ArticleStock_Produit
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`ArticleStock_Produit` (
-  `idArticleStock` INT NOT NULL,
-  `idProduit` INT NOT NULL,
-  PRIMARY KEY (`idArticleStock`, `idProduit`),
-  INDEX `FK_ArticleStock_Produit_idProduit_idx` (`idProduit` ASC) VISIBLE,
-  INDEX `fk_ArticleStock_Produit_idArticleStock_idx` (`idArticleStock` ASC) VISIBLE,
-  CONSTRAINT `FK_ArticleStock_Produit_ArticleStock`
-    FOREIGN KEY (`idArticleStock`)
-    REFERENCES `EasyService`.`ArticleStock` (`id`)
+CREATE TABLE IF NOT EXISTS EasyService.ArticleStock_Produit (
+  idArticleStock INT NOT NULL,
+  idProduit INT NOT NULL,
+  PRIMARY KEY (idArticleStock, idProduit),
+  INDEX FK_ArticleStock_Produit_idProduit_idx (idProduit ASC) VISIBLE,
+  INDEX fk_ArticleStock_Produit_idArticleStock_idx (idArticleStock ASC) VISIBLE,
+  CONSTRAINT FK_ArticleStock_Produit_ArticleStock
+    FOREIGN KEY (idArticleStock)
+    REFERENCES EasyService.ArticleStock (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `FK_ArticleStock_Produit_Produit`
-    FOREIGN KEY (`idProduit`)
-    REFERENCES `EasyService`.`Produit` (`id`)
+  CONSTRAINT FK_ArticleStock_Produit_Produit
+    FOREIGN KEY (idProduit)
+    REFERENCES EasyService.Produit (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
   
 
 
 -- -----------------------------------------------------
--- Table `EasyService`.`EcritureStock_LotArticle`
+-- Table EasyService.EcritureStock_LotArticle
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `EasyService`.`EcritureStock_LotArticle` (
-  `idEcritureStock_Log` INT NOT NULL,
-  `idEcritureStock_Commande` INT NOT NULL,
-  `idLotArticle` INT NOT NULL,
-  PRIMARY KEY (`idEcritureStock_Log`, `idEcritureStock_Commande`, `idLotArticle`),
-  INDEX `FK_EcritureStocks_LotArticle_idLotArticle_idx` (`idLotArticle` ASC) VISIBLE,
-  INDEX `FK_EcritureStock_LotArticle_idEcritureStock_idx` (`idEcritureStock_Log` ASC, `idEcritureStock_Commande` ASC) VISIBLE,
-  CONSTRAINT `FK_EcritureStock_LotArticle_idEcritureStock`
-    FOREIGN KEY (`idEcritureStock_Log` , `idEcritureStock_Commande`)
-    REFERENCES `EasyService`.`EcritureStock` (`idLog` , `idCommande`)
+CREATE TABLE IF NOT EXISTS EasyService.EcritureStock_LotArticle (
+  idEcritureStock_Log INT NOT NULL,
+  idEcritureStock_Commande INT NOT NULL,
+  idLotArticle INT NOT NULL,
+  PRIMARY KEY (idEcritureStock_Log, idEcritureStock_Commande, idLotArticle),
+  INDEX FK_EcritureStocks_LotArticle_idLotArticle_idx (idLotArticle ASC) VISIBLE,
+  INDEX FK_EcritureStock_LotArticle_idEcritureStock_idx (idEcritureStock_Log ASC, idEcritureStock_Commande ASC) VISIBLE,
+  CONSTRAINT FK_EcritureStock_LotArticle_idEcritureStock
+    FOREIGN KEY (idEcritureStock_Log , idEcritureStock_Commande)
+    REFERENCES EasyService.EcritureStock (idLog , idCommande)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `FK_EcritureStock_LotArticle_idLotArticle`
-    FOREIGN KEY (`idLotArticle`)
-    REFERENCES `EasyService`.`LotArticle` (`id`)
+  CONSTRAINT FK_EcritureStock_LotArticle_idLotArticle
+    FOREIGN KEY (idLotArticle)
+    REFERENCES EasyService.LotArticle (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
   
