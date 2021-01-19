88bfc8d2698110a1ef864ebd31d0bcb4481521c3
Hoang Anh Mai <hoanganh.mai@hes-so.ch>
2021-01-19 20:22:17 +0100

    views modified

--- assets/BDR - EasyService Views.sql
+++ assets/BDR - EasyService Views.sql
@@ -5,45 +5,75 @@ USE EasyService;
 -- -------------------- TABLES OUVERTES ----------------------
 CREATE VIEW vSalleTablesOuvertes
 AS
-	SELECT T.id, Produit.nom FROM TableSalle AS T
+	SELECT TableSalle.id, Produit.nom FROM TableSalle 
     INNER JOIN Commande 
 		INNER JOIN Commande_Produit
 			INNER JOIN Produit 
 			ON Produit.id = Commande_Produit.idProduit
 		ON Commande.id = Commande_Produit.idCommande
-	ON T.id = Commande.idTableSalle
-    WHERE T.ouvertFerme = 1; 
+	ON TableSalle.id = Commande.idTableSalle
+    WHERE TableSalle.ouvert = 1; 
     
 -- ----------- LISTE DE TABLES SERVIES PAR STAFFS ------------
 CREATE VIEW vSalleTablesParStaffs
 AS
-	SELECT T.id, Staff.nom, Staff.prenom FROM TableSalle AS T
+	SELECT TableSalle.id, Staff.nom, Staff.prenom FROM TableSalle
     INNER JOIN Commande 
 		INNER JOIN Staff 
         ON Staff.id = Commande.idStaff
-	ON T.id = Commande.idTableSalle;
+	ON TableSalle.id = Commande.idTableSalle;
 
 -- LISTE DE STAFFS PAR SERVICE (AVEC PRECISION DE L'HEURE DE TRAVAIL)
 CREATE VIEW vSalleStaffsParServices 
 AS
-	SELECT St.nom, St.prenom, HistoriqueStaff.debut AS arrivee, HistoriqueStaff.fin AS depart, Service.debut, Service.fin FROM Staff AS St
-    INNER JOIN Commande 
-		INNER JOIN Service 
-        ON Service.id = Commande.idService
-	ON St.id = Commande.idStaff
-    INNER JOIN HistoriqueStaff 
-    ON St.id = HistoriqueStaff.idStaff
+	SELECT 
+		Staff.nom, 
+        Staff.prenom, 
+        HistoriqueStaff.debut AS arrivee, 
+        HistoriqueStaff.fin AS depart, 
+        Service.debut, 
+        Service.fin 
+	FROM Staff
+		INNER JOIN Commande 
+			INNER JOIN Service 
+			ON Service.id = Commande.idService
+		ON Staff.id = Commande.idStaff
+		INNER JOIN HistoriqueStaff 
+		ON Staff.id = HistoriqueStaff.idStaff
     WHERE HistoriqueStaff.fin BETWEEN Service.debut AND Service.fin OR HistoriqueStaff.debut BETWEEN Service.debut AND Service.fin;	
 
 -- ================= VUE - CHEF CUISINIER ===================
 CREATE VIEW vCuisineStock
 AS
 	SELECT 
-		A.nom, 
-		L.nombrePortions, 
-        L.datePeremption 
-	FROM ArticleStock AS A, LotArticle AS L
-    WHERE A.id = L.idArticleStock;
+		ArticleStock.nom, 
+		LotArticle.nombrePortions, 
+        LotArticle.datePeremption 
+	FROM ArticleStock, LotArticle
+    WHERE ArticleStock.id = L.idArticleStock;
+    
+CREATE VIEW vCommande
+AS
+	SELECT 
+		Commande.id, 
+		Produit.nom 
+	FROM Commande
+		INNER JOIN Commande_Produit
+			INNER JOIN Produit 
+			ON Produit.id = Commande_Produit.idProduit
+		ON Commande.id = Commande_Produit.idCommande;
+    
+CREATE VIEW vProduit
+AS
+	SELECT
+		Produit.nom,
+        ArticleStock.nom
+	FROM Produit
+		INNER JOIN ArtcicleStock_Produit
+			INNER JOIN ArticleStock
+            ON ArticleStock_Produit = ArticleStock
+		ON Produit.id = ArticleStock_Produit.idProduit
+		
 
 		
 		
